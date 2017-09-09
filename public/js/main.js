1cc6f75d2db39213e87babf33362c25049ae790d
James Chhun <capt.wing.chhun@gmail.com>
2017-09-08 19:36:23 -0700

    Added responsiveness, fixed a lot of apdding issues involving header, fontsize for h1, and navbar not having room

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,39 @@
 //Start every Javascript document with doc.ready
-$(document).ready(function () {
-    $(window).scroll(function () {
-        //If 
-        if ($(window).scrollTop() > 970) {
-            //select nav bar, add bootstrap sticky header class
-            $('.navbar').addClass('fixed-top');
-            $('.navbar').css({
-                "backgroundColor": "rgba(0,0,0,0.5)"
-            });
-        }
-        if ($(window).scrollTop() < 971) {
-            //remove the navbar-fixed-top class
-            $('.navbar').removeClass('fixed-top');
-            $('.navbar').css({
-                "backgroundColor": "rgba(0,0,0,0.4)"
-            });
-        }
-    });
-    $(".burger-nav").on("click", function () {
-        $("header nav ul").toggleClass('open').delay(2000);
-    });
+$(document).ready(function() {
+  $(window).scroll(function() {
+    //If
+    if ($(window).scrollTop() > 970) {
+      //select nav bar, add bootstrap sticky header class
+      $('.navbar').addClass('fixed-top');
+      $('.navbar').css({
+        "backgroundColor": "rgba(0,0,0,0.5)"
+      });
+    }
+    if ($(window).scrollTop() < 971) {
+      //remove the navbar-fixed-top class
+      $('.navbar').removeClass('fixed-top');
+      $('.navbar').css({
+        "backgroundColor": "rgba(0,0,0,0.4)"
+      });
+    }
+  });
+  $(".burger-nav").on("click", function() {
+    $("header nav ul").toggleClass('open').delay(2000);
+    $(".header-row").toggleClass('move');
+  });
+
 });
-$(function () {
-    $('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 500);
-                return false;
-            }
-        }
-    });
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
 });
