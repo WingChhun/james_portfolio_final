df31134bace3319a683d3843e966567cd2f8e128
James Chhun <capt.wing.chhun@gmail.com>
2017-09-09 14:32:19 -0700

    No real changes, small markup error in index.ejs, tried to acdd particles to contact.ejs but failed

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,8 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
-particlesJS('particles-js',
-  
+particlesJS('particles-js'
+
   {
     "particles": {
       "number": {
